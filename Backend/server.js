45f911be072a7c27e679c52956015f9c81dda056
SahilDahiya99 <115144377+SahilDahiya997@users.noreply.github.com>
2023-10-08 19:25:06 +0530

    second commit

--- Backend/server.js
+++ Backend/server.js
@@ -22,11 +22,8 @@ app.get('/inventory-items', (req, res) => {
   res.status(200).json(inventory);
 });
 
-// Define a route to handle DELETE requests
-// Define a route to handle DELETE requests
 app.delete('/clear-items', (req, res) => {
-  // Logic to clear the data
-  // Clear the 'inventory' array of items
+  
   inventory.length = 0; // Clear the array without reassigning it
   res.status(204).send(); // Respond with a 204 No Content status on success
 });
