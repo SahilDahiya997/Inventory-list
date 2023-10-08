5cfe543f429d4d22ac085bd3684f3bfbeb636f16
SahilDahiya99 <115144377+SahilDahiya997@users.noreply.github.com>
2023-10-08 19:29:16 +0530

    final

--- Backend/server.js
+++ Backend/server.js
@@ -10,22 +10,19 @@ app.use(bodyParser.json());
 
 const inventory = [];
 
-// POST Endpoint for Adding Grocery Items
 app.post('/add-item', (req, res) => {
   const newItem = req.body;
   inventory.push(newItem);
   res.status(201).json({ message: 'Item added successfully' });
 });
 
-// GET Endpoint for Retrieving All Items
 app.get('/inventory-items', (req, res) => {
   res.status(200).json(inventory);
 });
 
 app.delete('/clear-items', (req, res) => {
-  
-  inventory.length = 0; // Clear the array without reassigning it
-  res.status(204).send(); // Respond with a 204 No Content status on success
+  inventory.length = 0;
+  res.status(204).send();
 });
 
 app.listen(PORT, () => {
